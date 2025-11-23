f5a5893c175f5d985dabc5b9b2f720646c55842f
TaewonKi <119064125+TaewonKi@users.noreply.github.com>
2025-11-23 08:37:33 +0000

    Refactor code structure for improved readability and maintainability

--- ocl_select/app/layout.tsx
+++ ocl_select/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        <SpeedInsights />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );

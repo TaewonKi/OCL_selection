4bd73269f89c213a143ffbe96e8bf367329ab502
TaewonKi <119064125+TaewonKi@users.noreply.github.com>
2025-11-23 08:19:02 +0000

    feat: integrate SpeedInsights component into layout

--- ocl_select/app/layout.tsx
+++ ocl_select/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
